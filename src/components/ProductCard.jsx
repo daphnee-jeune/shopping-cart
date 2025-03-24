a371ebb2bf69aae75969936573516e5412638807
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-03-24 11:24:47 -0500

    WIP - build product section layout

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div className="flex w-full flex-col overflow-hidden rounded-lg border border-primary/30 shadow-primary shadow-sm">
+      <div className="flex flex-col justify-center items-center overflow-hidden rounded-xl">
+        <img
+          src="Logo.png"
+          alt="Logo"
+          className="object-cover object-center hover:scale-125 transition-all duration-300"
+        />
+        <h3 className="text-xl mt-5 font-bold text-primary">Product title</h3>
+      </div>
+      <div className="mt-4 px-5 pb-5 flex flex-col items-center">
+      <div className="my-5 flex items-center justify-between gap-20">
+       <p className="text-2xl font-bold">Price</p>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/components/ProductSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MaxWidthWrapper from './MaxWidthWrapper'
+
+const ProductSection = () => {
+  return (
+    <div>
+      <MaxWidthWrapper>
+       <section className='grid grid-cols-3 gap-10 my-10 py-10'>
+        
+       </section>
+      </MaxWidthWrapper>
+    </div>
+  )
+}
+
+export default ProductSection
