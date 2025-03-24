55a857c7c9d44d89f6f4f1d0ed6e6db87f966f20
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-03-24 11:50:17 -0500

    implement routes logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import 'react'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-
   return (
-    <main className='flex flex-col min-h-screen bg-secondary font-montserrat text-gray-300'>
+    <main className="flex flex-col min-h-screen bg-secondary font-montserrat text-gray-300">
       <Navbar />
+      <Outlet />
       <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import router from './routes/router'
+import { RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<router />
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
