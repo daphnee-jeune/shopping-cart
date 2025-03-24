c7c3feaf61c501fbe78e1209615ba85bc9091f54
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-03-24 12:13:32 -0500

    Work on products state store

---
+++ src/store/useProductStore.js
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+const useProductStore = create((set) => ({
+  products: [],
+  loading: false,
+  error: null,
+
+  fetchProducts: async () => {
+    set({ loading: true, error: null });
+    try {
+      const response = await axios.get("https://dummyjson.com/products")
+      set({ products: response.data.products, loading: false })
+    } catch (err) {
+      set({ error: "An error occured while fetching the products", loading: false });
+    }
+  },
+}));
+
+export default useProductStore
