7370d5404f472fe06797b13f1713b3c191d25abe
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-03-24 11:37:38 -0500

    WIP 2 - build product section layout

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReviewStar from "./ReviewStar";
 
 const ProductCard = () => {
   return (
@@ -12,9 +13,13 @@ const ProductCard = () => {
         <h3 className="text-xl mt-5 font-bold text-primary">Product title</h3>
       </div>
       <div className="mt-4 px-5 pb-5 flex flex-col items-center">
-      <div className="my-5 flex items-center justify-between gap-20">
-       <p className="text-2xl font-bold">Price</p>
-      </div>
+        <div className="my-5 flex items-center justify-between gap-20">
+          <p className="text-2xl font-bold">Price</p>
+          <ReviewStar rating={4.5} />
+        </div>
+        <button className="rounded-full bg-primary hover:bg-primary/90 px-5 py-3 text-centr text-white">
+          Add to Cart
+        </button>
       </div>
     </div>
   );
--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
+import ProductCard from './ProductCard'
 
 const ProductSection = () => {
   return (
     <div>
       <MaxWidthWrapper>
        <section className='grid grid-cols-3 gap-10 my-10 py-10'>
-        
+        <ProductCard />
        </section>
       </MaxWidthWrapper>
     </div>
