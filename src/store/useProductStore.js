0ce4dd7e5589e3b64bb959889032d7b0a33bb04c
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-03-24 12:14:06 -0500

    formatting changes

--- src/store/useProductStore.js
+++ src/store/useProductStore.js
@@ -8,12 +8,15 @@ const useProductStore = create((set) => ({
   fetchProducts: async () => {
     set({ loading: true, error: null });
     try {
-      const response = await axios.get("https://dummyjson.com/products")
-      set({ products: response.data.products, loading: false })
+      const response = await axios.get("https://dummyjson.com/products");
+      set({ products: response.data.products, loading: false });
     } catch (err) {
-      set({ error: "An error occured while fetching the products", loading: false });
+      set({
+        error: "An error occured while fetching the products",
+        loading: false,
+      });
     }
   },
 }));
 
-export default useProductStore
+export default useProductStore;
