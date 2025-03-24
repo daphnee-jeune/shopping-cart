cf64e6b2c8485a900e91ff66792769e8bb5e3bd2
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-03-24 10:29:30 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'react'
+
+const App = () => {
+
+  return (
+    <main className=' flex flex-col min-h-screen bg-secondary font-montserrat text-gray-300'>
+      App
+    </main>
+  )
+}
+
+export default App
